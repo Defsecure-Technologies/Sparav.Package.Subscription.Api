79ec45096bb02dabbff25fe8737c3dd026dc1d80
Blerim Cazimi <blerim@burd.dk>
2020-08-05 16:20:08 +0200

    Fixes SPARAV_SUBSCRIPTION_API_AUTH_USERNAME

--- src/SubscriptionClientV1.php
+++ src/SubscriptionClientV1.php
@@ -7,7 +7,7 @@ class SubscriptionClientV1
 {
 
     public function subscription(string $subscription_id) {
-        $subscription = Http::withBasicAuth(env('SPARAV_SUBSCRIPTION_API_USERNAME'), env('SPARAV_SUBSCRIPTION_API_PASSWORD'))
+        $subscription = Http::withBasicAuth(env('SPARAV_SUBSCRIPTION_API_AUTH_USERNAME'), env('SPARAV_SUBSCRIPTION_API_AUTH_PASSWORD'))
             ->get("https://sparavsubscriptionapiprod.azurewebsites.net/api/v1/subscription/{$subscription_id}");
         return $subscription;
     }
