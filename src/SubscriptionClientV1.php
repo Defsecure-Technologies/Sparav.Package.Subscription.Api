d0850e692bf2666fae76bb7428b31c8616a97e56
Blerim Cazimi <blerim@burd.dk>
2020-08-25 14:53:15 +0200

    Update SubscriptionClientV1.php

--- src/SubscriptionClientV1.php
+++ src/SubscriptionClientV1.php
@@ -22,4 +22,27 @@ class SubscriptionClientV1
         return $add;
     }
 
+    /**
+     * @param int $customer_id
+     * @param int $subscription_type
+     * @return \Illuminate\Http\Client\Response
+     */
+    public function customersubscriptions(int $customer_id, int $subscription_type) {
+        $subscriptions = Http::withBasicAuth(env('SPARAV_SUBSCRIPTION_API_AUTH_USERNAME'), env('SPARAV_SUBSCRIPTION_API_AUTH_PASSWORD'))
+            ->get("https://sparavsubscriptionapiprod.azurewebsites.net/api/v1/customer/{$customer_id}/{$subscription_type}");
+        return $subscriptions;
+    }
+
+    /**
+     * Cancels the given subscription IDS.
+     * @param array $subscription_ids
+     * @return object
+     */
+    public function cancel(array $subscription_ids) {
+        $subscriptions = Http::withBasicAuth(env('SPARAV_SUBSCRIPTION_API_AUTH_USERNAME'), env('SPARAV_SUBSCRIPTION_API_AUTH_PASSWORD'))
+            ->post("https://sparavsubscriptionapiprod.azurewebsites.net/api/v1/subscription/cancel", ['subscription_ids' => $subscription_ids])->object();
+        return $subscriptions;
+    }
+
+
 }
